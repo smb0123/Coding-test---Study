78714603e020940556cab4f596d61e2ba9fb579d
devwqc <devwqc@gmail.com>
2024-01-17 00:08:07 +0900

    [프로그래머스] K번째수

---
+++ 정렬/[프로그래머스] K번째수.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    const start = commands[i][0] - 1;
+    const end = commands[i][1];
+    const pick = commands[i][2] - 1;
+    let slice = array.slice(start, end);
+    slice.sort((a, b) => a - b);
+    answer.push(slice[pick]);
+  }
+
+  return answer;
+}
