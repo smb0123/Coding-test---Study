3d702bd23e8ee6adba44cac3821a302e1a1bd41b
SIMMINBO <113810508+smb0123@users.noreply.github.com>
2024-02-08 15:04:41 +0900

    Revert "[심민보] 폰켓몬"

--- 해시/폰켓몬.js
+++
@@ -1,7 +0,0 @@
-function solution(nums) {
-  const set = [...new Set(nums)];
-  const num = nums.length / 2;
-  return set.length < num ? set.length : num;
-}
-
-console.log(solution([3, 3, 3, 2, 2, 2]));
