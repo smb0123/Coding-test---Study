a49f22e07a324a8d6439d11bbb103b49ceca249a
codingaring <sksms2104@naver.com>
2024-02-22 14:45:12 +0900

    전화번호 목록 문제 풀이

---
+++ 해시/phone_list.js
@@ -0,0 +1,14 @@
+function solution(phone_book) {
+  var answer = true;
+  const book_length = phone_book.length - 1; // 반복문에서 이용할 phone_book의 길이
+  phone_book.sort(); // phone_book 정렬 (길이가 짧은 애들 앞으로)
+
+  for (let i = 0; i < book_length; i++) {
+    let prefixLength = phone_book[i].length; //접두사가 되는지 확인할 번호의 길이
+
+    if (phone_book[i] === phone_book[i + 1].substring(0, prefixLength)) {
+      return false;
+    }
+  }
+  return answer;
+}
