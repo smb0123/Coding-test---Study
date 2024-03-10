f395e6eccfd2a0d03a10adeeee780ced57194c33
devwqc <devwqc@gmail.com>
2024-03-10 12:04:24 +0900

    [프로그래머스 Lv. 0] 몫 구하기 - 성공

---
+++ @프로그래머스/Lv. 0/몫 구하기.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+// 2024-03-10
+function solution(num1, num2) {
+  var answer = 0;
+
+  answer = Math.floor(num1 / num2);
+
+  return answer;
+}
