ad9c43690d07f16571f3d326975ca81a03977880
HaeJungg <gpwnd0120@gmail.com>
2024-03-13 11:17:04 +0900

    문자 반복 출력하기

---
+++ 프로그래머스/0단계/문자반복_출력하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, n) {
+  let result = "";
+
+  for (let i = 0; i < my_string.length; i++) {
+    for (let j = 0; j < n; j++) {
+      result += my_string[i];
+    }
+  }
+
+  return result;
+}
