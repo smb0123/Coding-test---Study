5c556af6be4ba38186975db09881ee8fc290151c
devwqc <devwqc@gmail.com>
2024-03-13 14:47:49 +0900

    [프로그래머스 Lv. 0] 피자 나눠 먹기 (3) #성공

---
+++ @프로그래머스/Lv. 0/피자 나눠 먹기 (3).js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120816
+
+// 2024-03-13
+// 성공
+function solution(slice, n) {
+  var answer = 0;
+
+  answer = Math.ceil(n / slice);
+
+  return answer;
+}
