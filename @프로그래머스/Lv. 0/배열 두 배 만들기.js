f8e1bf7c9c71af5552388b3f865b9146bb027a34
devwqc <devwqc@gmail.com>
2024-03-13 13:39:46 +0900

    [프로그래머스 Lv. 0] 배열 두 배 만들기 #성공

---
+++ @프로그래머스/Lv. 0/배열 두 배 만들기.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+// 2024-03-13
+function solution(numbers) {
+  var answer = [];
+
+  answer = numbers.map((number) => number * 2);
+
+  return answer;
+}
