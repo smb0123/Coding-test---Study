1db9ed22a6245d881ff841c226151c95e7738430
devwqc <devwqc@gmail.com>
2024-03-14 16:29:17 +0900

    [프로그래머스 Lv. 0] 옷가게 할인 받기 #성공

--- @프로그래머스/Lv. 0/옷가게 할인 받기.js
+++ @프로그래머스/Lv. 0/옷가게 할인 받기.js
@@ -31,3 +31,21 @@ function solution(price) {
 
   return answer;
 }
+
+// 2024-03-14
+// 성공
+function solution(price) {
+  var answer = 0;
+
+  if (price >= 500000) {
+    answer = Math.trunc(price * 0.8);
+  } else if (price >= 300000) {
+    answer = Math.trunc(price * 0.9);
+  } else if (price >= 100000) {
+    answer = Math.trunc(price * 0.95);
+  } else {
+    answer = price;
+  }
+
+  return answer;
+}
