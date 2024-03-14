24118278faebceba772208ef0b579373330eae44
codingaring <sksms2104@naver.com>
2024-03-14 18:03:04 +0900

    이어 붙인 수 문제 풀이 [완료]

---
+++ 0단계/이어 붙인 수.js
@@ -0,0 +1,21 @@
+function solution(num_list) {
+  let odd = [];
+  let even = [];
+
+  let oddString = "";
+  let evenString = "";
+
+  for (let i = 0; i < num_list.length; i++) {
+    num_list[i] % 2 !== 0 ? odd.push(num_list[i]) : even.push(num_list[i]);
+  }
+  console.log(odd, even);
+  for (let i = 0; i < odd.length; i++) {
+    oddString += odd[i];
+  }
+
+  for (let i = 0; i < even.length; i++) {
+    evenString += even[i];
+  }
+
+  return +evenString + +oddString;
+}
