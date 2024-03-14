7bdf26def918b1fb7db307315119a3bea6705cec
codingaring <sksms2104@naver.com>
2024-03-14 22:58:22 +0900

    옷가게 할인 받기 문제 풀이 [완료]

--- 0단계/옷가게 할인 받기.js
+++ 0단계/옷가게 할인 받기.js
@@ -2,11 +2,11 @@ function solution(price) {
   var answer = 0;
 
   if (price >= 500000) {
-    answer = price - Math.floor(price / 5);
+    answer = Math.floor(price - price / 5);
   } else if (price >= 300000) {
-    answer = price - Math.floor(price / 10);
+    answer = Math.floor(price - price / 10);
   } else if (price >= 100000) {
-    answer = price - Math.floor(price / 20);
+    answer = Math.floor(price - price / 20);
   } else {
     answer = price;
   }
