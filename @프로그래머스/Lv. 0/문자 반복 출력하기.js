8b1ffec32bd389484ca58053aac2f3321dc70b51
devwqc <devwqc@gmail.com>
2024-03-14 16:36:17 +0900

    [프로그래머스 Lv. 0] 문자 반복 출력하기 #성공

--- @프로그래머스/Lv. 0/문자 반복 출력하기.js
+++ @프로그래머스/Lv. 0/문자 반복 출력하기.js
@@ -12,3 +12,22 @@ function solution(my_string, n) {
 
   return answer;
 }
+
+// 2024-03-14
+// 성공
+function solution(my_string, n) {
+  var answer = '';
+
+  answer = my_string
+    .split('')
+    .map((string) => {
+      let str = string;
+      for (let i = 1; i < n; i++) {
+        str += string;
+      }
+      return str;
+    })
+    .join('');
+
+  return answer;
+}
