615a64c4d14ddce445dbac6d2591b5f8eecc97de
codingaring <sksms2104@naver.com>
2024-03-15 15:05:59 +0900

    숨어있는 숫자의 덧셈  풀이 [완료]

--- 0단계/숨어있는 숫자의 덧셈 (1).js
+++ 0단계/숨어있는 숫자의 덧셈 (1).js
@@ -1,10 +1,12 @@
 function solution(my_string) {
   var answer = 0;
 
-  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  let count = 0;
 
   for (let i = 0; i < my_string.length; i++) {
-    for (let j = 0; j < 8; j++) {
+    for (let j = 0; j < num.length; j++) {
       if (my_string[i] == num[j]) {
         answer += +my_string[i];
       }
