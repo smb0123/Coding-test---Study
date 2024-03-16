0844a6e0419a1de08e52ae00c6a548ccfb51fa42
devwqc <devwqc@gmail.com>
2024-03-16 10:48:49 +0900

    [프로그래머스 Lv. 0] n의 배수 #성공

---
+++ @프로그래머스/Lv. 0/n의 배수.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181937
+
+// 2024-03-16
+// 성공
+function solution(num, n) {
+  var answer = 0;
+
+  answer = num % n === 0 ? 1 : 0;
+
+  return answer;
+}
