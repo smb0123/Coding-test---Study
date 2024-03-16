69045d68fe85e0a20084fa4d2c2ee661921e6f9b
devwqc <devwqc@gmail.com>
2024-03-16 10:39:05 +0900

    [프로그래머스 Lv. 0] 정수 부분 #성공

---
+++ @프로그래머스/Lv. 0/정수 부분.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181850
+
+// 2024-03-16
+// 성공
+function solution(flo) {
+  var answer = 0;
+
+  answer = Math.trunc(flo);
+
+  return answer;
+}
