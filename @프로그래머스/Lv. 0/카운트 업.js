80cdd16f914ad337c378532ede967386dabb923a
devwqc <devwqc@gmail.com>
2024-03-18 22:35:30 +0900

    [프로그래머스 Lv. 0] 카운트 업 #성공

---
+++ @프로그래머스/Lv. 0/카운트 업.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+// 2024-03-18
+// 성공
+function solution(start_num, end_num) {
+  var answer = [];
+
+  for (let i = start_num; i <= end_num; i++) {
+    answer.push(i);
+  }
+
+  return answer;
+}
