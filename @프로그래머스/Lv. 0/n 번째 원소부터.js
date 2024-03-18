20d2a6ce8a5eb8854cb54c4915550b4a1cbf5b32
devwqc <devwqc@gmail.com>
2024-03-18 23:03:07 +0900

    [프로그래머스 Lv. 0] n 번째 원소부터 #성공

---
+++ @프로그래머스/Lv. 0/n 번째 원소부터.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181892
+
+// 2024-03-18
+// 성공
+function solution(num_list, n) {
+  var answer = [];
+
+  answer = num_list.slice(n - 1);
+
+  return answer;
+}
