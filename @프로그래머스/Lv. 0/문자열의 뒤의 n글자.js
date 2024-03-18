5e1ac4b085b8bb8e5714faa696eef69b504a57a1
devwqc <devwqc@gmail.com>
2024-03-18 22:33:16 +0900

    [프로그래머스 Lv. 0] 문자열의 뒤의 n글자 #성공

---
+++ @프로그래머스/Lv. 0/문자열의 뒤의 n글자.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181910
+
+// 2024-03-18
+// 성공
+function solution(my_string, n) {
+  var answer = '';
+
+  answer = my_string.slice(-n);
+
+  return answer;
+}
