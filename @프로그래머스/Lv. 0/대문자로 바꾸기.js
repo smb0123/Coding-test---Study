774219ca5c3517646b429b87cb5037e0cb2c3401
devwqc <devwqc@gmail.com>
2024-03-18 23:44:37 +0900

    [프로그래머스 Lv. 0] 대문자로 바꾸기 #성공

---
+++ @프로그래머스/Lv. 0/대문자로 바꾸기.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181877
+
+// 2024-03-18
+// 성공
+function solution(myString) {
+  var answer = '';
+
+  answer = myString.toUpperCase();
+
+  return answer;
+}
