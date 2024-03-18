b6b17ab8d315097d5921ad5c91dff5f51d8ed986
codingaring <sksms2104@naver.com>
2024-03-19 08:52:46 +0900

    순서 바꾸기 문제 풀이 스프레드 문법으로 변경 [완료]

--- 0단계/순서 바꾸기.js
+++ 0단계/순서 바꾸기.js
@@ -1,16 +1,8 @@
 function solution(num_list, n) {
   var answer = [];
 
-  const one = num_list.slice(n);
-  const two = num_list.splice(0, n);
-
-  for (let i = 0; i < one.length; i++) {
-    answer.push(one[i]);
-  }
-
-  for (let i = 0; i < two.length; i++) {
-    answer.push(two[i]);
-  }
+  answer.push(...num_list.slice(n));
+  answer.push(...num_list.splice(0, n));
 
   return answer;
 }
