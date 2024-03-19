33974e7b28dc823b47daf249a8fd8e1b5c3f6945
codingaring <sksms2104@naver.com>
2024-03-19 09:00:32 +0900

    A 강조하기 문제 풀이 [완료

---
+++ 0단계/A 강조하기.js
@@ -0,0 +1,15 @@
+function solution(myString) {
+  var answer = "";
+
+  for (let i = 0; i < myString.length; i++) {
+    if (myString[i] === "a") {
+      answer += "A";
+    } else if (myString[i] === "A") {
+      answer += myString[i];
+    } else {
+      answer += myString[i].toLowerCase();
+    }
+  }
+
+  return answer;
+}
