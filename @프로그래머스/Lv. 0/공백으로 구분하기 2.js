9f61e1b65bae2c6a2f909c250bdf9fbe9c3d9d30
devwqc <devwqc@gmail.com>
2024-03-20 23:25:26 +0900

    [프로그래머스 Lv. 0] 공백으로 구분하기 2 #성공

---
+++ @프로그래머스/Lv. 0/공백으로 구분하기 2.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181868
+
+// 2024-03-20
+// 성공
+function solution(my_string) {
+  var answer = [];
+
+  answer = my_string.split(' ').filter((str) => str);
+
+  return answer;
+}
