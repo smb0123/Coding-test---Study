656d9ac2da19a444c3c311de7eee7ee117c5816f
devwqc <devwqc@gmail.com>
2024-03-21 12:23:58 +0900

    [프로그래머스 Lv. 0] 최댓값 만들기(2) #성공

--- @프로그래머스/Lv. 0/최댓값 만들기(2).js
+++ @프로그래머스/Lv. 0/최댓값 만들기(2).js
@@ -16,3 +16,20 @@ function solution(numbers) {
 
   return answer;
 }
+
+// 2024-03-21
+// 성공
+function solution(numbers) {
+  var answer = 0;
+
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      max = numbers[i] * numbers[j] > max ? numbers[i] * numbers[j] : max;
+    }
+  }
+
+  answer = max;
+
+  return answer;
+}
