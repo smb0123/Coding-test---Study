28a90df9fce95f241cb71e04e3c2a201ea4d77b5
devwqc <devwqc@gmail.com>
2024-03-22 10:40:59 +0900

    [프로그래머스 Lv. 0] 이차원 배열 대각선 순회하기 #실패

---
+++ @프로그래머스/Lv. 0/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181829
+
+// 2024-03-22
+// 실패
+function solution(board, k) {
+  var answer = 0;
+
+  for (let i = 0; i < board.length; i++) {
+    if (i > k) break;
+    for (let j = 0; j < board[i].length; j++) {
+      if (i + j > k) {
+        break;
+      }
+      answer += i + j;
+    }
+  }
+
+  return answer;
+}
