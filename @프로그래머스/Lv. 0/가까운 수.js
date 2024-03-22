6801f474f51a10796376644bc598588fae77ec82
devwqc <devwqc@gmail.com>
2024-03-22 20:25:35 +0900

    [프로그래머스 Lv. 0] 가까운 수 #실패

---
+++ @프로그래머스/Lv. 0/가까운 수.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120890
+
+// 2024-03-22
+// 실패
+function solution(array, n) {
+  var answer = 0;
+
+  let min = 99;
+  for (let i = 0; i < array.length; i++) {
+    const gap = Math.abs(array[i] - n);
+    if (gap < min) {
+      min = gap;
+      answer = array[i];
+    }
+  }
+
+  return answer;
+}
