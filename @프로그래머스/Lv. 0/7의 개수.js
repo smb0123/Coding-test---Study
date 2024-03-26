7238a7070d551410b5a707be84f1e8be4f47159c
devwqc <devwqc@gmail.com>
2024-03-26 15:28:29 +0900

    [프로그래머스 Lv. 0] 7의 개수 #성공

---
+++ @프로그래머스/Lv. 0/7의 개수.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120912
+
+// 2024-03-26
+// 성공
+function solution(array) {
+  var answer = 0;
+
+  answer = array.reduce(
+    (acc, curr) => acc + curr.toString().split('7').length - 1,
+    0
+  );
+
+  return answer;
+}
