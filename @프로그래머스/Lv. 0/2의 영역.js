196baa4ff941aa24c7cfbeca671e3cf0b4f27da4
devwqc <devwqc@gmail.com>
2024-03-26 15:07:04 +0900

    [프로그래머스 Lv. 0] 2의 영역 #성공

---
+++ @프로그래머스/Lv. 0/2의 영역.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181894
+
+// 2024-03-26
+// 성공
+function solution(arr) {
+  var answer = [];
+
+  const start = arr.indexOf(2);
+  const end = arr.lastIndexOf(2);
+
+  if (start === -1) {
+    return [-1];
+  }
+
+  answer = arr.filter((_, i) => i >= start && i <= end);
+
+  return answer;
+}
