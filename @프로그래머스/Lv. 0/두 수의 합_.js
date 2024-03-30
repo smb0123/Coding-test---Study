0acc9c23ca04b43086b965a56f837b063b81b41a
devwqc <devwqc@gmail.com>
2024-03-30 16:16:01 +0900

    [프로그래머스 Lv. 0] 두 수의 합_ #성공

---
+++ @프로그래머스/Lv. 0/두 수의 합_.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181846
+
+// 2024-03-30
+// 성공
+function solution(a, b) {
+  var answer = '';
+
+  answer = String(BigInt(a) + BigInt(b));
+
+  return answer;
+}
