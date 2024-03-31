3993d3bc60b0d17813c7b249a332c780ee0e3b40
devwqc <devwqc@gmail.com>
2024-03-31 13:04:14 +0900

    [프로그래머스 Lv. 0] 구슬을 나누는 경우의 수 #실패

---
+++ @프로그래머스/Lv. 0/구슬을 나누는 경우의 수.js
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120840
+
+// 2024-03-31
+// 실패
+function solution(balls, share) {
+  var answer = 0;
+
+  function factorial(n) {
+    if (n === 1) return 1;
+
+    return n * factorial(n - 1);
+  }
+
+  answer = factorial(balls) / (factorial(balls - share) * factorial(share));
+
+  return answer;
+}
